89c45c6ac3d30fffd242abcbd32c4a0c0c8aff64
hatlonely <hatlonely@foxmail.com>
2024-11-18 16:59:07 +0000

    update

--- src/doc.rs
+++ src/doc.rs
@@ -1,4 +1,5 @@
 mod conversion;
+mod expressions;
 mod helloworld;
 mod primitives;
 mod structures;
---
+++ src/doc/expressions.rs
@@ -0,0 +1,18 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn expressions() {
+        let x = 5;
+        let y = {
+            let x_squared = x * x;
+            let x_cube = x_squared * x;
+            x_cube + x_squared + x
+        };
+
+        println!("x: {}", x);
+        println!("y: {}", y);
+
+        assert_eq!(x, 5);
+        assert_eq!(y, 155);
+    }
+}
