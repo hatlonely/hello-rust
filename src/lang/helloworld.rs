2d7234a33e8988535011c1d82e46c0087cccd180
hatlonely <hatlonely@foxmail.com>
2024-11-18 14:44:36 +0000

    update

--- src/lang/helloworld.rs
+++ src/lang/helloworld.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn main() {
+    fn helloworld() {
         println!("Hello, world!");
     }
 }
--- src/lang/primitives.rs
+++ src/lang/primitives.rs
@@ -3,7 +3,7 @@ mod tests {
     #[test]
     #[allow(unused_variables)]
     #[allow(unused_assignments)]
-    fn main() {
+    fn literals() {
         let ok: bool = true;
         let b = false;
 
@@ -31,5 +31,62 @@ mod tests {
 
         let mut x = 1;
         x = 2;
+    }
+
+    #[test]
+    fn tuples() {
+        let t = (1u8, 2u32);
+        println!("t: {:?}", t);
+        println!("t.0: {:?}", t.0);
+        println!("t.1: {:?}", t.1);
+
+        let tt = ((1, 2), (3, 4));
+        println!("tt: {:?}", tt);
+        println!("tt.0.0: {:?}", tt.0 .0);
+        println!("tt.0.1: {:?}", tt.0 .1);
+        println!("tt.1.0: {:?}", tt.1 .0);
+        println!("tt.1.1: {:?}", tt.1 .1);
+
+        let (a, b) = t;
+        println!("a: {:?}", a);
+        println!("b: {:?}", b);
+    }
+
+    #[test]
+    fn arrays() {
+        let a = [1, 2, 3];
+        println!("a: {:?}", a);
+        println!("a[0]: {:?}", a[0]);
+        println!("a[1]: {:?}", a[1]);
+        println!("a[2]: {:?}", a[2]);
+        println!("a.len(): {:?}", a.len());
+
+        for i in 0..a.len() {
+            println!("a[{}]: {:?}", i, a[i]);
+        }
+
+        let mut m = [0; 3];
+        println!("m: {:?}", m);
+        m[0] = 1;
+        m[1] = 2;
+        m[2] = 3;
+        println!("m: {:?}", m);
+    }
+
+    #[test]
+    fn slices() {
+        let a = [1, 2, 3, 4, 5];
+        let s = &a[1..4];
+        println!("s: {:?}", s);
+        println!("s[0]: {:?}", s[0]);
+        println!("s[1]: {:?}", s[1]);
+        println!("s[2]: {:?}", s[2]);
+
+        for i in 0..a.len() {
+            match s.get(i) {
+                Some(x) => println!("a[{}]: {:?}", i, x),
+                None => println!("a[{}]: None", i), // 超过范围的索引返回 None
+            }
+        }
     }
 }
