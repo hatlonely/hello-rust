b1b50df7a210367e80a4efa2a760fb5177d53ede
hatlonely <hatlonely@foxmail.com>
2024-11-18 16:26:22 +0000

    update

--- src/doc/types.rs
+++ src/doc/types.rs
@@ -14,4 +14,42 @@ mod tests {
             print!("Casting: {} -> {}", decimal, integer);
         }
     }
+
+    #[test]
+    fn literals() {
+        let x = 1u8;
+        let y = 2u32;
+        let z = 3f32;
+
+        let i = 1;
+        let f = 1.0;
+
+        println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
+        println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+        println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+        println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+        println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
+    }
+
+    #[test]
+    fn inference() {
+        let elem = 5u8;
+
+        let mut vec = Vec::new();
+        vec.push(elem);  // 根据 `elem` 推断 `vec` 的类型
+        println!("{:?}", vec);
+    }
+
+    #[test]
+    fn aliasing() {
+        type NanoSecond = u64;
+        type Inch = u64;
+
+        // 使用别名
+        let nanoseconds: NanoSecond = 5 as u64;
+        let inches: Inch = 2 as u64;
+
+        // 类型别名不提供额外的类型安全，因为别名不提供新的类型
+        println!("{} nanoseconds + {} inches = {} unit?", nanoseconds, inches, nanoseconds + inches);
+    }
 }
