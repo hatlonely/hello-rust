d770a536d6e9ea1ac7ecb9d17e1fa2de3be062bb
hatlonely <hatlonely@foxmail.com>
2024-11-23 18:53:59 +0000

    update

--- bin/jsonrpsee/http.rs
+++ bin/jsonrpsee/http.rs
@@ -1,2 +1,24 @@
-use jsonrpsee::http_server::HttpServerBuilder;
-use jsonrpsee::http_server::RpcModule;
+use jsonrpsee::server::{RpcModule, Server};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+struct Message {
+    message: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let server = Server::builder().build("127.0.0.1:8081").await?;
+
+    let mut module = RpcModule::new(());
+    module.register_method("say_hello", |_, _, _| {
+        println!("say_hello method called!");
+        "Hello there!!"
+    })?;
+
+    let handler = server.start(module);
+
+    handler.stopped().await;
+
+    Ok(())
+}
