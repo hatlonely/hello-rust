0bb1336566f94a4a29abbcfad7fc5f37cecf0217
hatlonely <hatlonely@foxmail.com>
2024-11-24 15:02:29 +0000

    update

--- bin/actix-web/server.rs
+++ bin/actix-web/server.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -14,12 +15,28 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+#[derive(Deserialize)]
+struct Info {
+    name: String,
+    age: u8,
+}
+
+#[get("/info")]
+async fn info(query: web::Query<Info>) -> impl Responder {
+    let info = query.into_inner();
+    HttpResponse::Ok().body(format!("Name: {}, Age: {}", info.name, info.age))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            // curl -XGET "127.0.0.1:8080"
             .service(hello)
+            // curl -XPOST -d "hello world" "127.0.0.1:8080/echo"
             .service(echo)
+            // curl -XGET "127.0.0.1:8080/info?name=John&age=20"
+            .service(info)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
