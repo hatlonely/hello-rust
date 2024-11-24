faaff53ec5e8ceafc7942a0613e83401fec7980d
hatlonely <hatlonely@foxmail.com>
2024-11-24 15:34:26 +0000

    update

--- bin/actix-web/server.rs
+++ bin/actix-web/server.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 
 #[get("/")]
@@ -15,7 +15,7 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Info {
     name: String,
     age: u8,
@@ -25,6 +25,28 @@ struct Info {
 async fn info(query: web::Query<Info>) -> impl Responder {
     let info = query.into_inner();
     HttpResponse::Ok().body(format!("Name: {}, Age: {}", info.name, info.age))
+}
+
+#[derive(Deserialize, Debug)]
+struct Message {
+    message: String,
+}
+
+#[post("/handler")]
+async fn handler(
+    req: HttpRequest,
+    query: web::Query<Info>,
+    body: web::Json<Message>,
+) -> impl Responder {
+    println!("uri: {:?}", req.uri());
+    println!("path: {:?}", req.path());
+    println!("headers: {:?}", req.headers());
+    println!("query: {:?}", query.into_inner());
+    println!("headers: {:?}", req.headers());
+    let message = body.into_inner();
+    println!("body: {:?}", &message);
+
+    HttpResponse::Ok().body(message.message)
 }
 
 #[actix_web::main]
@@ -37,6 +59,8 @@ async fn main() -> std::io::Result<()> {
             .service(echo)
             // curl -XGET "127.0.0.1:8080/info?name=John&age=20"
             .service(info)
+            // curl -XPOST -H "Content-Type: application/json" -d '{"message": "hello world"}' "127.0.0.1:8080/handler?name=John&age=20"
+            .service(handler)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
