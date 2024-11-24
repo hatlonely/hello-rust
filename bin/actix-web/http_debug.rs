0c7a1d2b9f0b2840df76c546d998fb45d6a947c9
hatlonely <hatlonely@foxmail.com>
2024-11-24 16:00:58 +0000

    update

---
+++ bin/actix-web/http_debug.rs
@@ -0,0 +1,22 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn index(req: HttpRequest, req_body: String) -> impl Responder {
+    println!("====================================");
+    println!("uri: {:?}", req.uri());
+    println!("headers:");
+    for (name, value) in req.headers() {
+        println!("  {}: {:?}", name, value);
+    }
+    println!("body: {}", req_body);
+    println!("====================================");
+
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().default_service(web::route().to(index)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
